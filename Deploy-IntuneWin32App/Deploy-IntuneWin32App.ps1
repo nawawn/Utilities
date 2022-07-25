c5f949b06cde01971aa0946a7bd2a7d33ef4cdc2
Naw Awn <nawawn@googlemail.com>
2022-07-25 21:22:08 +0100

    added Deploy-IntuneWin32App

---
+++ Deploy-IntuneWin32App/Deploy-IntuneWin32App.ps1
@@ -0,0 +1,98 @@
+﻿#Requires -Modules Microsoft.Graph.Intune, IntuneWin32App, MSAL.PS
+
+Function Deploy-IntuneWinApp{
+    Param(
+        [Parameter(Mandatory)]
+        [String]$PSConfigFile = ".\IntuneWin32App-Config.psd1"
+    )
+    Process{
+        If (-Not(Test-Path -Path $PSConfigFile)){
+            Write-Warning "Configuration File Path not found!"
+            return
+        }
+        If (".psd1" -ne (Get-ChildItem -Path $PSConfigFile).Extension){
+            Write-Warning "Configuration File needs to be in psd1 format"
+            return
+        }
+        If (-Not(Connect-MSGraph)){
+            Connect-MSGraph
+        }
+        If (-Not($Global:AccessToken)){
+            Connect-MSIntuneGraph
+        }
+        $Config = Import-PowerShellDataFile -Path $PSConfigFile
+        Foreach ($App in $Config.Keys){
+            Write-Verbose "$App - Setting up the application!"
+            If ($Config[$App].ODataType -ne '#microsoft.graph.win32LobApp'){
+                Write-Warning "$App - This type of application is not supported!"
+                Continue
+            }
+            If (".intunewin" -ne (Get-ChildItem -Path $($Config[$App].FilePath)).Extension){
+                Write-Warning "$App - File is not in the corret format. Expected .INTUNEWIN file!"
+                Continue
+            }
+            Write-Verbose "$App - Setting up the Detection Rule"
+            Switch($Config[$App].DetectionRule.Type){
+                'Script' {
+                    $ScriptRule = $($Config[$App].DetectionRule.Script)
+                    $DetectionRule = New-IntuneWin32AppDetectionRuleScript @ScriptRule
+                    break
+                }
+                'MSI' {
+                    $MsiRule = $($Config[$App].DetectionRule.MSI)
+                    $DetectionRule = New-IntuneWin32AppDetectionRuleMSI @MsiRule
+                    break
+                }
+                'File' {
+                    $FileRule = $($Config[$App].DetectionRule.File)
+                    $DetectionRule = New-IntuneWin32AppDetectionRuleFile @FileRule
+                    break
+                }
+                'Registry' {
+                    $RegistryRule  = $($Config[$App].DetectionRule.Registry)
+                    $DetectionRule = New-IntuneWin32AppDetectionRuleRegistry @RegistryRule
+                    break
+                }
+                Default { Write-Warning "$App - Unknown Detection Rule Type" }           
+            }
+            If (-Not($DetectionRule)){
+                Write-Warning "$App - Failed to create the detection Rule"
+                Continue
+            }
+
+            Write-Verbose "$App - Creating the Application on Intune" 
+            $NewApp = Add-IntuneWin32App -DisplayName $App -FilePath $Config[$App].FilePath -Description $Config[$App].Description `
+                        -Publisher $Config[$App].Publisher -InstallExperience $Config[$App].InstallExp `
+                        -InstallCommandLine $Config[$App].InstallCmd -UninstallCommandLine $Config[$App].UninstallCmd `
+                        -RestartBehavior $Config[$App].RestartMode -DetectionRule $DetectionRule 
+            
+            Start-Sleep 2
+            
+            If (-Not($NewApp.Id)){
+                Write-Warning "$App - Failed to create the application on Intune"
+                Continue
+            }
+
+            Write-Verbose "$App - Assigning the Group and Intent"
+            If ($Config[$App].Assignment.Group){
+                $AssignGroup = $($Config[$App].Assignment.Group)
+                Add-IntuneWin32AppAssignmentGroup -ID $NewApp.Id @AssignGroup
+            }
+            If ($Config[$App].Assignment.AllUsers){
+                $AllUsers = $($Config[$App].Assignment.AllUsers)
+                Add-IntuneWin32AppAssignmentAllUsers -ID $NewApp.Id @AllUsers
+            }
+            If ($Config[$App].Assignment.AllDevicess){
+                $AllDevices = $($Config[$App].Assignment.AllDevices)
+                Add-IntuneWin32AppAssignmentAllDevices -ID $NewApp.Id @AllDevices
+            }      
+            $DetectionRule = $null
+            $NewApp        = $null
+            $AssignGroup   = $null
+            $AllUsers      = $null
+            $AllDevices    = $null            
+        }
+    
+    }
+
+}
---
+++ Deploy-IntuneWin32App/IntuneWin32App-Config.psd1
@@ -0,0 +1,74 @@
+﻿@{
+    #'Example of Win32 App for Intune'
+    Orca = @{
+        ODataType    = '#microsoft.graph.win32LobApp'
+        FilePath     = 'C:\Temp\OrcaMSI\Orca.intunewin'
+        Description  = 'Orca MSI Explorer'
+        Publisher    = 'Microsoft'
+        InstallCmd   = 'msiexec /i Orca.msi /qn'
+        UninstallCmd = 'msiexec /x {85F4CBCB-9BBC-4B50-A7D8-E1106771498D} /qn'
+        InstallExp   = 'system'
+        RestartMode  = 'suppress'
+        DetectionRule = @{
+            Type = 'MSI'
+            Script = @{
+                ScriptFile = 'c:\temp\Check-MyApplication.ps1'
+                RunAs32Bit = $false
+                EnforceSignatureCheck = $false
+            }
+            MSI = @{
+                ProductCode = '{85F4CBCB-9BBC-4B50-A7D8-E1106771498D}'
+            }
+            File = @{
+                Existence = $true
+                Path      = 'C:\Program Files (x86)\Orca'
+                FileOrFolder  = 'Orca.exe'
+                DetectionType = 'exists'
+            }
+            Registry = @{
+                Existence = $true
+                KeyPath   = 'HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{85F4CBCB-9BBC-4B50-A7D8-E1106771498D}'
+                ValueName = 'UninstallString'
+                DetectionType = 'exists'
+            }           
+        }
+        Assignment = @{
+            Group = @{
+                Include = $true
+                GroupID = '6388083c-6a27-4dcd-a96d-80e8c9d3324d'
+                Intent  = 'available'
+            }           
+        }
+    }
+    'Microsoft ODBC SQL Driver' = @{
+        ODataType    = '#microsoft.graph.win32LobApp'
+        FilePath     = 'C:\Temp\Microsoft\msodbcsql.intunewin'
+        Description  = 'Microsodft ODBC SQL'
+        Publisher    = 'Microsoft'
+        InstallCmd   = 'msiexec /i msodbcsql.msi /qn'
+        UninstallCmd = 'msiexec /x {7453C0F5-03D5-4412-BB8F-360574BE29AF} /qn'        
+        InstallExp   = 'system'
+        RestartMode  = 'suppress'
+        DetectionRule = @{
+            Type = 'MSI'
+            MSI = @{
+                ProductCode = '{7453C0F5-03D5-4412-BB8F-360574BE29AF}'
+            }
+        }
+        Assignment = @{
+            Group = @{
+                Include = $true
+                GroupID = '5b4ec678-c1fd-4f89-9338-91b28493f5b9'
+                Intent  = 'required'
+            }
+            AllUsers = @{
+                Intent = 'available'
+            }
+            AllDevices = @{
+                Intent = 'available'
+            }
+        }
+    }
+}
+
+#$Config.Keys
